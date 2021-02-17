fac420e6d33bd63017b717aa4e434c284ba2795d
Veronica De Leon <v.dleon@gmail.com>
2021-02-17 15:45:49 +0100

    request ao api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useState, useEffect }  from 'react';
 import './App.css';
+import axios from "axios"
 
-function App() {
+const App = () => {
+
+  const [gridArr, setGridArr] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await axios
+      .get("https://api.unsplash.com/photos/?client_id=NqnV0VzStxxn1EOyvsMOn7HKN_J7r4gYZk1kgvlL164")
+      .then(response => {
+        //console.log(response)
+        setGridArr(response.data)
+      })
+      .catch(err => console.log(err))
+    }
+    fetchData()
+  }, [])
+
+  console.log(gridArr)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h3>Hello</h3>
+
     </div>
   );
 }
